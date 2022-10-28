ababc8243f916a414e917b23e0094d24eb247723
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2022-10-28 11:02:18 -0400

    Fix

--- gitlab-error-processor/app.py
+++ gitlab-error-processor/app.py
@@ -1,8 +1,9 @@
 import json
 from pathlib import Path
-from fastapi import FastAPI, Request
-from kubernetes import client, config
+
 import yaml
+from fastapi import FastAPI, HTTPException, Request, Response
+from kubernetes import client, config
 
 config.load_incluster_config()
 
@@ -19,29 +20,33 @@ async def grafana_endpoint():
 
 @app.post("/")
 async def gitlab_webhook_consumer(request: Request):
-    # TODO: This endpoint will receive the gitlab webhook for failed jobs and
-    # create a k8s job to parse the logs and upload them to opensearch.
-    with open(Path(__file__).parent / 'job-template.yaml') as f:
-        job_template = yaml.safe_load(f)
-
     job_input_data = await request.json()
 
-    for container in job_template['spec']['template']['spec']['containers']:
+    if job_input_data.get("object_kind", "") != "build":
+        raise HTTPException(status_code=400, detail="Invalid request")
+
+    if job_input_data["build_status"] != "failure":
+        return Response("Not a failed job, no action needed.", status_code=200)
+
+    # TODO: This endpoint will receive the gitlab webhook for failed jobs and
+    # create a k8s job to parse the logs and upload them to opensearch.
+    with open(Path(__file__).parent / "job-template.yaml") as f:
+        job_template = yaml.safe_load(f)
+
+    for container in job_template["spec"]["template"]["spec"]["containers"]:
         container.setdefault("env", []).extend(
-            [
-                dict(name="JOB_INPUT_DATA", value=json.dumps(job_input_data))
-            ]
+            [dict(name="JOB_INPUT_DATA", value=json.dumps(job_input_data))]
         )
 
-    job_template['metadata']['name'] = "gitlab-error-processing-job"
+    job_template["metadata"]["name"] = "gitlab-error-processing-job"
 
-    # Make sure to add labels to make finding the job that proccessed the error log
-    # easier.
-    job_template['metadata']['labels'] = {
-        'spack.io/gitlab-job-id': '{{job_id}}',
-        'spack.io/other-useful-annotation': '...'
+    # Make sure to add labels to make finding the job that proccessed the
+    # error log easier.
+    job_template["metadata"]["labels"] = {
+        "spack.io/gitlab-job-id": "22222",  # '{{job_id}}',
+        "spack.io/other-useful-annotation": "usefulAnnotation",
     }
 
     # TODO:  make sure to add a namespace for running these jobs in
-    batch.create_namespaced_job('gitlab-error-processor', job_template)
-    return "ok"
+    batch.create_namespaced_job("gitlab-error-processor", job_template)
+    return Response("Upload job dispatched.", status_code=202)
